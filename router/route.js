45dceb3b0e5e9bf4e66218e48b57bf593e9e4914
Hugo Prenat <hugo.prenat@epitech.eu>
2017-03-12 20:49:59 +0100

    Change tree view

---
+++ router/route.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const middlewares = require('./middleware/middleware.js');
+
+// Our router with which we define each route.
+const router = express.Router();
+
+// Default behavior for any routes
+router.use(function(req, res, next) {
+    // Authentication of users using NARMS server.
+
+    // Ensure to pass through all routes.
+    next();
+});
+
+// Default middleware.
+router.get('/', middlewares.default);
+
+// middleware handling login event.
+router.post('/login', middlewares.login);
+
+// middleware handling logout event.
+router.post('/logout', middlewares.logout);
+
+// middleware handling roleChange event.
+router.post('/roleChange', middlewares.roleChange);
+
+// Export the router.
+module.exports = router;
